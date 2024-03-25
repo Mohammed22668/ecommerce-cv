4e20b795ba90dc5ce08dc75b4f2a941c15c149c6
Mohammed <ezio.ahmed12@gmail.com>
2024-03-26 02:17:45 +0300

    update server and checkout page

--- app/(home)/checkout/page.jsx
+++ app/(home)/checkout/page.jsx
@@ -1,18 +1,31 @@
 "use client";
-import React from "react";
+import React, { Suspense } from "react";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import CheckoutForm from "./_components/CheckoutForm";
 import { useSearchParams } from "next/navigation";
 
-const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHER_KEY);
-const page = () => {
+const stripePromise = loadStripe(
+  "pk_test_51OuueWFhYrmB04TdbJyC1JoX1zYTBOxUZKTfeaYtLe9K6BEFc8MSOm9HP5CUff7Ky0KXIgTlRxQPwYVNamdUT47300gVyL4uD7"
+);
+
+const Page = () => {
+  // Wrap useSearchParams() in Suspense boundary
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <CheckoutFormWithSearchParams />
+    </Suspense>
+  );
+};
+
+const CheckoutFormWithSearchParams = () => {
   const searchParams = useSearchParams();
   const options = {
     mode: "payment",
     currency: "usd",
     amount: Number(searchParams.get("amount")) * 100,
   };
+
   return (
     <Elements stripe={stripePromise} options={options}>
       <CheckoutForm amount={Number(searchParams.get("amount"))} />
@@ -20,4 +33,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Page;
